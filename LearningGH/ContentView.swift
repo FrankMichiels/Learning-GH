f7e5b3f3ad50f54c3e4c51672197b7709179423f
FrankMichiels <101967646+FrankMichiels@users.noreply.github.com>
2022-03-20 10:52:51 +0100

    Update ContentView.swift

--- LearningGH/ContentView.swift
+++ LearningGH/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Change done in GitHub!")
+        Text("Hello, 2nd Change done in GitHub!")
             .padding()
     }
 }
